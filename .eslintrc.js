32cd88e1187483ef69619e512e9b4b0ad893b2e4
TBioucas <tiago.bioucas@gmail.com>
2020-10-27 22:18:47 +0000

    Setup files;

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  plugins: ['prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+  ignorePatterns: [
+    'configs/**',
+    'dist/**',
+    'node_modules/**',
+    '**/__tests__/**',
+  ],
+};
