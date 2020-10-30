2bdfb246b07b059bec6058378df3b142aeb31361
TBioucas <tiago.bioucas@gmail.com>
2020-10-30 18:52:42 +0000

    Added @storybook/addon-actions;

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
-	stories: ['../src/components/**/*.stories.js'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+	stories: [
+		"../src/**/*.stories.mdx",
+		"../src/**/*.stories.@(js|jsx)"
+	],
+	addons: [
+		"@storybook/addon-actions",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/preset-create-react-app"
+	],
 };
