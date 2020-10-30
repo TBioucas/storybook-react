e6fcc4f452f9f251123f3cb007048b2e53973ec2
TBioucas <tiago.bioucas@gmail.com>
2020-10-30 19:43:24 +0000

    Added @storybook/addon-controls;

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,8 @@ module.exports = {
 		"../src/**/*.stories.@(js|jsx)"
 	],
 	addons: [
-		"@storybook/addon-actions",
+    "@storybook/addon-actions",
+    "@storybook/addon-controls",
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/preset-create-react-app"
